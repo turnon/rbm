e0896b9093019d1191f7c5fb9ba5f3db380ace26
z <z@cent.com>
2015-08-21 17:21:45 +0800

    help to create db if the given db file does not exists or is empty

--- sqlite_conn.rb
+++ sqlite_conn.rb
@@ -3,7 +3,15 @@ require 'active_record'
 
 class SqliteConn
   def self.establish(path)
+    check_db path
     ActiveRecord::Base.establish_connection(:adapter  => 'sqlite3', :database => path)
+  end
+
+  def self.check_db(path)
+    File.exists?(path) || File.new(path, 'w').close
+    if File.size(path).zero?
+      `sqlite3 #{path} 'CREATE TABLE "bookmark_files" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar);CREATE TABLE "categories" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar, "add" datetime, "last_modified" datetime, "category_id" integer, "bookmark_file_id" integer); CREATE TABLE "links" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar, "add" datetime, "href" varchar, "category_id" integer); CREATE INDEX "index_categories_on_bookmark_file_id" ON "categories" ("bookmark_file_id"); CREATE INDEX "index_categories_on_category_id" ON "categories" ("category_id"); CREATE INDEX "index_links_on_category_id" ON "links" ("category_id");'`
+    end
   end
 end
 
