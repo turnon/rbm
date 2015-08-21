f5e4aba744b45fa42c712fbc7bdb7bca31c69f26
ken <block24block@gmail.com>
2015-08-21 11:29:44 +0800

    mv dir_stack into bookmark_file

--- anly.rb
+++ anly.rb
@@ -1,32 +1,38 @@
-require_relative 'dir_stack'
 require_relative 'sqlite_conn'
 require_relative 'bookmark_file'
 require_relative 'category'
 require_relative 'link'
 
-SqliteConn.establish 'db/development.sqlite3'
+SqliteConn.establish ARGV.pop
 
-st = DirStack.new
+while not ARGV.empty?
 
-while line = gets
-  line.sub!(/^\s+/, '')
-  #next unless /(\<D|\<\/D)/ =~ line
-  if Link.match? line
-    l = Link.new
-    l.parse line
-    puts "#{st.cur ? st.cur.name : 'root'} #{l.name} #{l.href} #{l.add}"
-  elsif Category.match? line
-    c = Category.new
-    c.parse line
-    puts "#{st.cur ? st.cur.name : 'root'} #{c.name} #{c.add} #{c.last_modified}"
-    st << c
-  elsif Category.match_ending? line
-    st.getout
+  file_path = ARGV.shift
+
+  bmf = BookmarkFile.new
+  bmf.name = File.basename(file_path, '.html')
+
+  File.open(file_path) do |f|
+
+    while line = f.gets
+      line.sub!(/^\s+/, '')
+      if Link.match? line
+        l = Link.new
+        l.parse line
+        puts "#{bmf.current_category ? bmf.current_category.name : 'root'} #{l.name} #{l.href} #{l.add}"
+      elsif Category.match? line
+        c = Category.new
+        c.parse line
+        puts "#{bmf.current_category ? bmf.current_category.name : 'root'} #{c.name} #{c.add} #{c.last_modified}"
+        bmf.getin c
+      elsif Category.match_ending? line
+        bmf.getout
+      end
+    end
+
   end
+
+  bmf.save
+
 end
-
-#st.each do |t| 
-#  puts t
-#end
-
 
