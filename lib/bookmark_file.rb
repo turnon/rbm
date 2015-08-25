baf0ecb6d031dae204d37dbba31ebb98dd2f3c28
ken <block24block@gmail.com>
2015-08-25 23:43:57 +0800

    get categories recusively

--- lib/bookmark_file.rb
+++ lib/bookmark_file.rb
@@ -16,4 +16,17 @@ class BookmarkFile < ActiveRecord::Base
     @st.pop
   end
 
+  alias_method :cs, :categories
+
+  def categories(*opt)
+    opt[0] == :r ? categories_recurse : cs
+  end
+
+  def categories_recurse
+    cs = []
+    r = -> c { cs << c ; subcs = c.categories ; subcs.each &r unless subcs.empty? }
+    categories.each &r
+    cs
+  end
+
 end
