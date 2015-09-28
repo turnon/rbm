ccc6c00182b30496039dab96d1d037c28c920c7d
ken <ken@bogon.(none)>
2015-09-28 23:21:30 +0800

    class method branch(=) to get or set filter

--- lib/bookmark_file.rb
+++ lib/bookmark_file.rb
@@ -18,4 +18,25 @@ class BookmarkFile < ActiveRecord::Base
     @st.pop
   end
 
+  class << self
+
+    def branch=(b)
+      alias_method :o_categories, :categories unless instance_methods.any? { |m| m == :o_categories }
+      @@p = b.sub(/^\//,'').split(/\//)
+      define_method :categories do
+        @start_point = self.o_categories[0]
+        @@p.each do |b_name|
+          @start_point = @start_point.categories.select{ |c| c.name == b_name }[0]
+          break if @start_point.nil?
+        end
+        @start_point
+      end
+    end
+
+    def branch
+      @@p
+    end
+
+  end
+
 end
