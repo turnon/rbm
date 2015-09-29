6e09857f894c6dd34e4883298cb5bf4d0283c29e
ken <ken@bogon.(none)>
2015-09-29 23:31:48 +0800

    avoid repetitive search

--- lib/bookmark_file.rb
+++ lib/bookmark_file.rb
@@ -23,15 +23,26 @@ class BookmarkFile < ActiveRecord::Base
     def branch=(b)
       alias_method :o_categories, :categories unless instance_methods.any? { |m| m == :o_categories }
       b = b.sub(/^\//,'').split(/\//)
+
       return if class_variable_defined? :@@branch and @@branch == b
+
       @@branch = b
+      @@branch_changed = true
+
       define_method :categories do
-        stp = self.o_categories
-        @@branch.each do |b_name|
-          stp = stp[0].categories.select{ |c| c.name == b_name }
-          break if stp.empty?
-        end
-        @start_point = stp
+
+	if @@branch_changed or not instance_variable_defined? :@start_point
+          stp = self.o_categories
+          @@branch.each do |b_name|
+            stp = stp[0].categories.select{ |c| c.name == b_name }
+            break if stp.empty?
+          end
+          @start_point = stp
+          @@branch_changed = false
+	end
+
+	@start_point
+
       end
     end
 
